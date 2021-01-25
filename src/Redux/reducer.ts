bd9f10e190484ccccc9ceba1397a3d2dbb96766b
NathanThomasWebDeveloper <hellonathanthomas@gmail.com>
2021-01-26 00:31:28 +0100

    refactor add createReducer

--- src/Redux/reducer.ts
+++ src/Redux/reducer.ts
@@ -1,8 +1,9 @@
-import {Tstate, Taction} from "../Config/TypeScript/Types";
-import {IS_LOADING, FETCH_WEATHER_DATA, SET_LENGTH_UNIT, SET_TEMPERATURE_UNIT, SHOW_ERROR_MESSAGE} from "./actionTypes";
+import {Ttemperature_unit, Tlength_unit} from "../Config/TypeScript/Types";
 // any change to state should be reflected in config/TypeScript/types file.
 import initialState from "./initialState";
-
+import {createReducer} from "@reduxjs/toolkit";
+import {setTemperatureUnit, setLengthUnit, showError} from './actions';
+/*
 const reducer = (state = initialState, action: Taction): Tstate => {
     const {payload}: { payload: any } = action;
     switch (action.type) {
@@ -20,4 +21,18 @@ const reducer = (state = initialState, action: Taction): Tstate => {
             return state;
     }
 }
+*/
+
+const reducer = createReducer(initialState, builder => {
+    builder.addCase(setTemperatureUnit, (state, action) => {
+        state.temperature_unit = (action.payload as Ttemperature_unit)
+    })
+    builder.addCase(setLengthUnit, (state, action) => {
+        state.length_unit = (action.payload as Tlength_unit)
+    })
+    builder.addCase(showError, (state, action) => {
+        state.error = action.payload
+    })
+})
+
 export default reducer;
